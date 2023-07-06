b4da612574275816d915c5f4d0b3c17d056544c9
ViliIlieva <levi@abv.bg>
2023-07-06 22:22:02 +0300

    06/07

--- 03. JS Objects and Classes/Exercises/01. Employees.js
+++ 03. JS Objects and Classes/Exercises/01. Employees.js
@@ -1,10 +1,16 @@
-function solve(input){
-    let employees = {};
-    for (const line of input) {
-        let name = line;
-
-        employees[name] = name.length;
-    }
+function solve(input) {
+    // let employees = {};
+    // for (const line of input) {
+    //     employees[line] = line.length;
+    // }
+    //
+    // for (const key in employees) {
+    //     console.log(`Name: ${key} -- Personal Number: ${employees[key]}`);
+    // }
+    let employees = input.reduce((data, emploee) => {//първо приемаме data във която ще редуцираме и текущото employee
+        data[emploee] = emploee.length;//закачаме ключ и стойност
+        return data;//връщаме новото състояние на data
+    }, {});//като втори аргумент получаваме мястото където ще редуцираме всичко {} т.е празен обект
 
     for (const key in employees) {
         console.log(`Name: ${key} -- Personal Number: ${employees[key]}`);
--- 03. JS Objects and Classes/Exercises/02. Towns.js
+++ 03. JS Objects and Classes/Exercises/02. Towns.js
@@ -1,11 +1,11 @@
-function solve(input){
+function solve(input) {
 
     for (const line of input) {
         let [town, latitude, longitude] = line.split(' | ');
         latitude = Number(latitude).toFixed(2);
         longitude = Number(longitude).toFixed(2);
-        let towns = {town, latitude, longitude};
-        console.log(towns);
+        let townObj = {town, latitude, longitude};
+        console.log(townObj);
     }
 }
 
