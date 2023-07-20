381c88f34328c3539946aff1cc849879b9b68214
ViliIlieva <levi@abv.bg>
2023-07-20 23:16:28 +0300

    20/07

--- 05-JS-HTTP-and-REST/Exercises/02.Bus-Schedule/app.js
+++ 05-JS-HTTP-and-REST/Exercises/02.Bus-Schedule/app.js
@@ -1,10 +1,32 @@
 function solve() {
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/';
+    let stopID = 'depot';
+    let nameStop = '';
+
+    let info = document.querySelector('.info');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+
     function depart() {
-        // TODO: 
+        fetch(`${BASE_URL}${stopID}`)
+            .then(res => res.json())
+            .then((busStop) => {
+                let {name, next} = busStop
+                nameStop = name;
+                info.textContent = `Next stop ${name}`;
+                stopID = next;
+                departBtn.disabled = true;
+                arriveBtn.disabled = false;
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     async function arrive() {
-        // TODO:
+        info.textContent = `Arriving at ${nameStop}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
