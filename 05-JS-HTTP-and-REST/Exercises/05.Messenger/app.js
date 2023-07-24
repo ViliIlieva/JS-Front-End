fdf01cf2910d654d61d007e1d0c0be906b619ede
ViliIlieva <levi@abv.bg>
2023-07-24 17:25:20 +0300

    24/07

--- 05-JS-HTTP-and-REST/Exercises/05.Messenger/app.js
+++ 05-JS-HTTP-and-REST/Exercises/05.Messenger/app.js
@@ -1,5 +1,39 @@
 function attachEvents() {
-    // TODO:
+    const authorInput = document.querySelector('[name="author"]');
+    const contentInput = document.querySelector('[name="content"]');
+    const BASE_URL = 'http://localhost:3030/jsonstore/messenger';
+    const textarea = document.getElementById('messages');
+
+    document.getElementById('submit').addEventListener('click', sendMessage);
+    document.getElementById('refresh').addEventListener('click', refreshMessage);
+
+    function refreshMessage() {
+        textarea.disable = false;
+        fetch(BASE_URL)
+            .then(res => res.json())
+            .then((messages) => {
+                textarea.innerHTML = '';
+                for (const key in messages) {
+                    let{author, content} = messages[key];
+                    textarea.append(`${author}: ${content}\n`)
+                }
+            })
+    }
+
+    function sendMessage() {
+        let author = authorInput.value;
+        let content = contentInput.value;
+        fetch(BASE_URL, {
+            method: 'POST',
+            body: JSON.stringify({author, content})
+        })
+            .then(res => res.json())
+            .then(() => {
+                refreshMessage();
+                authorInput.value = '';
+                contentInput.value = '';
+            })
+    }
 }
 
 attachEvents();
